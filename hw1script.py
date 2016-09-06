07292eddc46f2a78f942d59f36c1546c04fef395
Mauricio Salazar Giraldo <msalaza8@masonlive.gmu.edu>
2016-09-06 17:29:48 -0400

    Something is not working properly with python, gives syntax errors when trying to insert comments and when printing out words. The first output is the total X, the second is the total Y, the third is the end effector.

--- hw1script.py
+++ hw1script.py
@@ -3,28 +3,29 @@
 
 import math
 
+
 def EndEffectorCalculator(a1, a2, a3):
   
   xTotal = float(1*math.cos(rad1) + 3*math.cos(rad1 + rad2) + 2*math.cos(rad1 + rad2 + rad3))
+  print
   print(xTotal)
 
   yTotal =  float(1*math.sin(rad1) + 3*math.sin(rad1 + rad2) + 2*math.sin(rad1 + rad2 + rad3))
   print(yTotal)
 
-  
+  EndEffector = math.sqrt(xTotal*xTotal + yTotal*yTotal)
+  print
+  print(EndEffector)
+  print
 
 
 x = raw_input()
 angle1 = float(x)
 rad1 = math.radians(angle1)
 
-
-
 y = raw_input()
 angle2 = float(y)
 rad2 = math.radians(angle2)
-
-
 
 z = raw_input()
 angle3 = float(z)
