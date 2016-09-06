cab8e28005d45326a93265006efc8271062fa4f5
Mauricio Salazar Giraldo <msalaza8@masonlive.gmu.edu>
2016-09-06 17:17:17 -0400

    Changed the structure of the program, added a function and the Y equation.

--- hw1script.py
+++ hw1script.py
@@ -3,29 +3,36 @@
 
 import math
 
+def EndEffectorCalculator(a1, a2, a3):
+  
+  xTotal = float(1*math.cos(rad1) + 3*math.cos(rad1 + rad2) + 2*math.cos(rad1 + rad2 + rad3))
+  print(xTotal)
+
+  yTotal =  float(1*math.sin(rad1) + 3*math.sin(rad1 + rad2) + 2*math.sin(rad1 + rad2 + rad3))
+  print(yTotal)
+
+  
+
 
 x = raw_input()
 angle1 = float(x)
 rad1 = math.radians(angle1)
-print(rad1)
-angle1 = (math.cos(math.radians(angle1)))
-print(angle1)
+
+
 
 y = raw_input()
 angle2 = float(y)
 rad2 = math.radians(angle2)
-print(rad2)
-angle2 = (math.cos(math.radians(angle2)))
-print(angle2)
+
+
 
 z = raw_input()
 angle3 = float(z)
 rad3 = math.radians(angle3)
-print(rad3)
-angle3 = (math.cos(math.radians(angle3)))
-print(angle3)
+
+
+EndEffectorCalculator(rad1, rad2, rad3)
 
 
 
 
-xTotal = float(1*math.cos(rad1) + 3*math.cos(rad1 + rad2) + 2*math.cos(rad1 + rad2 + rad3))
